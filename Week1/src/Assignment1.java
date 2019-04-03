cf66eb868100368800b75c1d4b40ec4e08b904e5
Chelsea J <43831285+ChelseaChristine@users.noreply.github.com>
2019-04-02 19:04:07 -0700

    adds first assignment

---
+++ Week1/src/Assignment1.java
@@ -0,0 +1,24 @@
+//draws the example on 3G.21
+import java.awt.*;
+
+public class Assignment1 {
+	public static void main(String[] args) {
+	
+		DrawingPanel panel = new DrawingPanel(500,500);
+		panel.setBackground(Color.YELLOW);
+		Graphics face = panel.getGraphics();
+		//creates circles for ears
+		face.setColor(Color.BLUE);
+		face.fillOval(100, 100, 100, 100);
+		face.fillOval(300, 100, 100, 100);
+		
+		//creates box
+		face.setColor(Color.RED);
+		face.fillRect(150, 150, 200, 200);
+		
+		//creates line
+		face.setColor(Color.BLACK);
+		face.drawLine(150, 250, 350, 250);
+	}
+
+}

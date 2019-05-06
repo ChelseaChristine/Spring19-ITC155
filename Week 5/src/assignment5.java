356e0739a6a80ed392c012f8a11caa1e42d9672b
Chelsea J <43831285+ChelseaChristine@users.noreply.github.com>
2019-05-06 14:39:23 -0700

    adds comments to the test runs of the method

--- Week 5/src/assignment5.java
+++ Week 5/src/assignment5.java
@@ -2,11 +2,14 @@
 public class assignment5 {
 
 	public static void main(String[] args) {
+		//tests to make sure it will print atleast 1
 		writeNums(1);
 		System.out.println();
+		//two tests to make sure it rans as needed
 		writeNums(5);
 		System.out.println();
 		writeNums(30);
+		//When uncommented, the below method will throw out the IllegalArgumentException
 		//writeNums(0);
 		
 
